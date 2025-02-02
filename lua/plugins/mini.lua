6a439ccb61f542db3dabb8b8a472a1353fe6504a
Maulana Ahmad Aji Triadi <maulanaajk@gmail.com>
2025-02-02 22:26:09 +0700

    feat: add mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,14 +2,15 @@ return {
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
-      require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
+        require('mini.ai').setup { n_lines = 500 }
+        require('mini.surround').setup()
+        require('mini.pairs').setup()
 
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = true }
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+        local statusline = require 'mini.statusline'
+        statusline.setup { use_icons = true }
+        ---@diagnostic disable-next-line: duplicate-set-field
+        statusline.section_location = function()
+            return '%2l:%-2v'
+        end
     end,
 }
