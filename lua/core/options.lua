e0cc4c8ed523d2fe4745a5d048a98b24eb2dddf0
Maulana Ahmad Aji Triadi <maulanaajk@gmail.com>
2025-02-12 01:35:03 +0700

    chore: better options with folding

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,27 +1,43 @@
 local opt = vim.opt
 
+-- Line numbers & scrolling
 opt.number = true
 opt.relativenumber = true
-opt.mouse = 'a'
-opt.showmode = false
-opt.undofile = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.cursorline = true
-opt.wrap = false
 opt.scrolloff = 10
 
-opt.clipboard = 'unnamedplus'
+-- UI settings
+opt.cursorline = true
+opt.wrap = false
+opt.showmode = false
+opt.mouse = 'a'
 
+-- Searching
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Splits
 opt.splitright = true
 opt.splitbelow = true
 
+-- Persistent undo
+opt.undofile = true
+
+-- List characters
 opt.list = true
 opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- shell
-opt.shell = 'zsh'
+-- Clipboard
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
 
-if vim.fn.has 'win32' == 1 or vim.fn.has 'win64' == 1 then
-  opt.shell = 'pwsh'
-end
+-- Folding
+opt.foldmethod = 'expr'
+opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+opt.foldlevel = 99
+
+-- Terminal
+opt.termguicolors = true
+
+-- Shell selection
+opt.shell = vim.loop.os_uname().sysname:find 'Windows' and 'pwsh' or 'zsh'
