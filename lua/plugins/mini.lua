aa3fead5081312a519c4bcb41595eb285509aa67
Maulana Ahmad Aji Triadi <maulanaajk@gmail.com>
2025-04-22 01:06:26 +0700

    feat(mini): add jsx and tsx commenting

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,17 +1,31 @@
 return {
-  'echasnovski/mini.nvim',
-  version = '*',
-  config = function()
-    require('mini.comment').setup()
-    require('mini.ai').setup { n_lines = 500 }
-    require('mini.surround').setup()
-    require('mini.pairs').setup()
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+  {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+      require('mini.comment').setup({
+        options = {
+          custom_commentstring = function()
+            return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
+          end,
+        }
+      })
+      require('mini.ai').setup { n_lines = 500 }
+      require('mini.surround').setup()
+      require('mini.pairs').setup()
 
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
-  end,
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = true }
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end,
+  }
 }
