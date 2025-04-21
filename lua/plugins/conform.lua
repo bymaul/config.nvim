d6c385862faaf33f8170b472ba5b8823c09f90e3
Maulana Ahmad Aji Triadi <maulanaajk@gmail.com>
2025-04-22 01:05:41 +0700

    fix: `prettierd` not formatting
    i use kickstart config instead

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,26 +1,44 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function()
-      return {
-        timeout_ms = 500,
-        lsp_format = 'fallback',
-      }
-    end,
-  },
-  formatters_by_ft = {
-    javascript = { 'prettierd' },
-    typescript = { 'prettierd' },
-    javascriptreact = { 'prettierd' },
-    typescriptreact = { 'prettierd' },
-    css = { 'prettierd' },
-    html = { 'prettierd' },
-    json = { 'prettierd' },
-    yaml = { 'prettierd' },
-    markdown = { 'prettierd' },
-    lua = { 'stylua' },
-    php = { 'pint', 'php_cs_fixer' },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format { async = true, lsp_format = "fallback" }
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = "fallback",
+          }
+        end
+      end,
+      formatters_by_ft = {
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        lua = { "stylua" },
+        php = { "pint", "php_cs_fixer" },
+      },
+    },
   },
 }
